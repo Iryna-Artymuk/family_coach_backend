d938f33325269599c9a53a31d3a58ab12bdff6bd
Iryna Artymuk <artimyk@ukr.net>
2023-11-23 09:18:49 +0200

    add feedback data

---
+++ feedbackData.js
@@ -0,0 +1,116 @@
+module.exports =[
+    {
+        "date": "2023-11-16T16:31:11.542Z",
+        "body": "occaecati recusandae expedita",
+        "name": "Mallory",
+        "id": "1"
+    },
+    {
+        "date": "2023-11-17T03:29:28.974Z",
+        "body": "Autem nobis molestiae maxime. Vero aperiam sed labore. Tenetur architecto corrupti voluptatem dignissimos harum ut exercitationem suscipit non. Sint unde quae voluptates nesciunt quaerat.\nIpsum ex modi id esse similique iste velit. Dolore tempore expedita cumque nisi fugiat inventore molestias. Non ad magnam a. Iure optio maxime animi debitis fugiat illo. Ducimus voluptates cupiditate. Dolore dolore voluptates ea amet.\nEius dolorem quo assumenda unde sint. Fugiat consectetur officia neque aut nesciunt. Ex atque aliquam. Consequuntur pariatur quisquam enim debitis. Animi magni error voluptatem odio est.",
+        "name": "Marcelle",
+        "id": "2"
+    },
+    {
+        "date": "2023-11-17T04:20:53.979Z",
+        "body": "Nemo hic cumque voluptates tempora. Nihil praesentium praesentium rem laborum dicta odio explicabo. Alias fugit ipsa ratione iste aperiam soluta nulla. Dicta non incidunt sint magni. Fugiat consequatur quae dicta blanditiis iste nulla.",
+        "name": "Elian",
+        "id": "3"
+    },
+    {
+        "date": "2023-11-17T13:24:06.218Z",
+        "body": "Nam quae cum harum voluptate voluptatum.\nPorro dicta nam.\nPraesentium est officia quibusdam cum enim inventore.",
+        "name": "Torey",
+        "id": "4"
+    },
+    {
+        "date": "2023-11-22T18:40:52.685Z",
+        "body": "Placeat iste facere nihil dolor qui reprehenderit et quam. Labore saepe iusto nobis autem molestiae. Dolore voluptates quis voluptatibus necessitatibus. Eos exercitationem et corporis autem autem velit eius sunt voluptate. Ea sapiente voluptate laborum maiores.\nAlias non eius sit error laborum exercitationem ratione doloremque. Aperiam at autem fuga maiores nemo cumque alias nihil facere. Quaerat architecto numquam. Assumenda possimus libero repellat odit aliquam quaerat. Illum ipsum quo. Quasi nesciunt magni et dolorum earum.\nAspernatur ullam quod commodi accusamus blanditiis. Nulla ex minima nisi expedita officiis dolorum inventore eveniet. Sint magni velit veniam praesentium eos ipsum eos laboriosam. Quis tempore expedita molestiae deserunt. Magnam magni ex.",
+        "name": "Alva",
+        "id": "5"
+    },
+    {
+        "date": "2023-11-21T23:00:44.512Z",
+        "body": "unde perspiciatis doloribus",
+        "name": "Allene",
+        "id": "6"
+    },
+    {
+        "date": "2023-11-22T06:44:41.486Z",
+        "body": "Aliquam nulla quod ipsa necessitatibus porro labore. Ipsam ab maiores maxime occaecati facere cumque. Atque neque quasi quia dicta dolorem laboriosam voluptatem tempore. In maiores necessitatibus. Eius reprehenderit quis dolor consequatur repudiandae ea nemo vero maiores. Qui necessitatibus laborum rem labore aspernatur possimus ipsam.",
+        "name": "Ellis",
+        "id": "7"
+    },
+    {
+        "date": "2023-11-22T07:36:12.744Z",
+        "body": "Inventore voluptatum possimus ad sequi illum molestiae nam saepe officiis. Optio quos itaque aliquid veniam cumque corrupti voluptatem modi. Tempore iure quidem voluptatibus dolore quos. Dolorem nisi accusamus harum ab adipisci eveniet cum laboriosam.",
+        "name": "Kiley",
+        "id": "8"
+    },
+    {
+        "date": "2023-11-21T21:26:26.170Z",
+        "body": "dolorem illum labore",
+        "name": "Missouri",
+        "id": "9"
+    },
+    {
+        "date": "2023-11-22T19:59:46.178Z",
+        "body": "Reiciendis ad commodi excepturi. Voluptatibus inventore officiis iure ad. Quas ducimus culpa recusandae. Temporibus accusamus perferendis. Quis accusamus voluptatum adipisci aliquid. Optio fugit quidem.",
+        "name": "Fausto",
+        "id": "10"
+    },
+    {
+        "date": "2023-11-22T17:04:12.140Z",
+        "body": "voluptatem illo facere",
+        "name": "Cali",
+        "id": "11"
+    },
+    {
+        "date": "2023-11-22T06:47:15.680Z",
+        "body": "cupiditate",
+        "name": "Jana",
+        "id": "12"
+    },
+    {
+        "date": "2023-11-22T17:34:06.824Z",
+        "body": "eaque",
+        "name": "Roosevelt",
+        "id": "13"
+    },
+    {
+        "date": "2023-11-22T09:46:51.833Z",
+        "body": "Ea ipsam aperiam ipsum beatae. Quae odio minus alias neque ut. Ipsa odio modi vitae voluptas quo tempore iste recusandae provident. Fugit delectus sed provident aspernatur.",
+        "name": "Patrick",
+        "id": "14"
+    },
+    {
+        "date": "2023-11-22T02:46:11.377Z",
+        "body": "eum",
+        "name": "Monroe",
+        "id": "15"
+    },
+    {
+        "date": "2023-11-22T02:13:56.136Z",
+        "body": "eveniet",
+        "name": "Yvette",
+        "id": "16"
+    },
+    {
+        "date": "2023-11-22T06:52:03.389Z",
+        "body": "Ad perferendis qui vel fuga ex facilis consequatur.",
+        "name": "Derick",
+        "id": "17"
+    },
+    {
+        "date": "2023-11-22T02:43:16.378Z",
+        "body": "laboriosam sapiente rerum",
+        "name": "Corbin",
+        "id": "18"
+    },
+    {
+        "date": "2023-11-22T00:23:16.822Z",
+        "body": "repellat",
+        "name": "Jakob",
+        "id": "19"
+    }
+]
