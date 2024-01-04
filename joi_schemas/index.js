51e08479712651d419f98ca2657bca1a84355be7
Iryna Artymuk <artimyk@ukr.net>
2024-01-04 20:45:36 +0200

    avatar schema

--- joi_schemas/index.js
+++ joi_schemas/index.js
@@ -5,3 +5,4 @@ export { default as qualificationSchema } from './qualificationSchema.js';
 export { default as adultPriceSchema } from './price/adultPriceSchema.js';
 export { default as kidsPriceSchema } from './price/kidsPriceSchema.js';
 export { default as lecturePriceSchema } from './price/lecturePriceSchema.js';
+// export { default as userAvatarSchema } from './userAvatarSchema.js';
--- middlewars/auth/avatarValidation.js
+++ middlewars/auth/avatarValidation.js
@@ -2,8 +2,6 @@ import validateBody from '../../decorators/validateBody.js';
 import userAvatarSchema from '../../joi_schemas/UserAvatarSchema.js';
 
 
-
-
 const vadidateAvatar = validateBody(userAvatarSchema);
 
 export default vadidateAvatar;
